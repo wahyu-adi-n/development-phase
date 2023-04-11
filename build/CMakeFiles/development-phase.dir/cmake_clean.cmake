8d3d99cee3db3b9b3314300d85e75a8c6123d0cb
Wahyu Adi Nugroho <wahyuadinugrohoasinan@gmail.com>
2023-04-11 21:36:55 +0700

    make predictions or inference on c++

---
+++ build/CMakeFiles/development-phase.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/development-phase.dir/infer_model.cpp.o"
+  "development-phase"
+  "development-phase.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/development-phase.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
